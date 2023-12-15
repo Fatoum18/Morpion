037b0dce42239d45f4bfcf06c0f5b1db1f77e8a3
Fatoum18 <fatimadiallofd488@gmail.com>
2023-12-15 03:43:05 -0500

    creation des models User, Game et GameConfig

--- Morpion/models.py
+++ Morpion/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=200, default="")
+
+class GameConfig(models.Model):
+    id =  models.AutoField(primary_key=True)
+    grid_size = models.IntegerField(default=3)
+    alignment = models.IntegerField(default=3)
+    created_at =  models.DateField(default=datetime.now, blank=True) 
+    
+      
+class Game(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    visibility = models.CharField(max_length=20, default="PUBLIC")
+    created_at = models.DateField(default=datetime.now, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    config = models.ForeignKey(GameConfig, on_delete=models.CASCADE)
+
