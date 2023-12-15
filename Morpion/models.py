ccb1a70c9f74f96ef3e4e458f669309fbb220d8f
Fatoum18 <fatimadiallofd488@gmail.com>
2023-12-15 04:15:03 -0500

    Ajout de la propriete access_code sur le model game

--- Morpion/models.py
+++ Morpion/models.py
@@ -19,6 +19,7 @@ class Game(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     visibility = models.CharField(max_length=20, default="PUBLIC")
+    access_code = models.CharField(max_length=20)
     created_at = models.DateField(default=datetime.now, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     config = models.ForeignKey(GameConfig, on_delete=models.CASCADE)
